81c02fb96bdb240d738a14c0e71f3ba672b39f10
Mr.chu <chuhongkyu@gmail.com>
2023-12-02 23:13:26 +0900

    Add Environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { createGlobalStyle} from 'styled-components';
-import ToDoList from './components/ToDoList';
-
-const GlobalStyle = createGlobalStyle`
-* {
-  font-family: 'Roboto', sans-serif;
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${prop =>prop.theme.bgColor};
-  padding: 10px;
-  width: 100%;
-  height: 100vh;
-  overflow: hidden;
-}
-`
+import React from 'react';
+import logo from './logo.svg';
 
 function App() {
   return (
-    <>
-      <GlobalStyle/>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={ <ToDoList/>}/>
-        </Routes>
-      </BrowserRouter>
-    </>  
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RecoilRoot } from 'recoil';
-import { ThemeProvider } from 'styled-components';
+import './index.css';
 import App from './App';
-import { theme } from './theme';
+import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <App/>
-      </ThemeProvider>
+  <React.StrictMode>
+   <RecoilRoot>
+      <App/>
     </RecoilRoot>
-  
+  </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
